97b386e43b5419c0cf7312d425071d54db6a138d
Chavez <matthew@el-chavez.me>
2018-10-31 21:06:10 -0400

    fix: Lint errors

--- jumpconshash.go
+++ jumpconshash.go
@@ -1,10 +1,15 @@
 package jumpconshash
 
+// OFFSET constant for hashing
 var OFFSET uint64 = 2862933555777941757
-var MAXINT float64 = float64(int64(1) << 31)
 
+// MAXINT is the max int size allowed
+var MAXINT = float64(int64(1) << 31)
+
+// Hash takes an uint64 key and a total number of buckets to consider to hash
+// the key into one of the buckets consistently
 func Hash(key uint64, buckets int) (b int64) {
-	var j int64 = 0
+	var j int64
 	b = -1
 	for j < int64(buckets) {
 		b = j
